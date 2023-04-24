1022eddd0ef4b9b666a629160e6de6c748e3fe48
hoangnv <hoangnv@ai-t.vn>
2023-04-24 14:56:24 +0700

    first commit

--- sw.js
+++ sw.js
@@ -22,6 +22,15 @@ self.addEventListener('notificationclick', function(event) {
   event.notification.close();
 
   event.waitUntil(
-    clients.openWindow('https://developers.google.com/web')
+    clients
+      .matchAll({
+        type: "window",
+      })
+      .then((clientList) => {
+        for (const client of clientList) {
+          if (client.url === "/" && "focus" in client) return client.focus();
+        }
+        if (clients.openWindow) return clients.openWindow("/");
+      })
   );
 });
